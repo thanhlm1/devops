e9cc48363f03e338b40a5b7d4230fa8dc49566a6
thanhlm1 <thanh.lm@st.buv.edu.vn>
2022-11-07 21:18:44 +0700

    fix dockerfile

--- web0/assets/js/custom.js
+++ web0/assets/js/custom.js
@@ -34,84 +34,34 @@
 	    $("header").removeClass("background-header");
 	  }
 	});
+	
+	$('.filters ul li').click(function(){
+        $('.filters ul li').removeClass('active');
+        $(this).addClass('active');
+          
+          var data = $(this).attr('data-filter');
+          $grid.isotope({
+            filter: data
+          })
+        });
 
+        var $grid = $(".grid").isotope({
+          	itemSelector: ".all",
+          	percentPosition: true,
+          	masonry: {
+            columnWidth: ".all"
+        }
+    })
 
-
-	const Accordion = {
-		settings: {
-		  // Expand the first item by default
-		  first_expanded: false,
-		  // Allow items to be toggled independently
-		  toggle: false
-		},
-  
-		openAccordion: function(toggle, content) {
-		  if (content.children.length) {
-			toggle.classList.add("is-open");
-			let final_height = Math.floor(content.children[0].offsetHeight);
-			content.style.height = final_height + "px";
-		  }
-		},
-  
-		closeAccordion: function(toggle, content) {
-		  toggle.classList.remove("is-open");
-		  content.style.height = 0;
-		},
-  
-		init: function(el) {
-		  const _this = this;
-  
-		  // Override default settings with classes
-		  let is_first_expanded = _this.settings.first_expanded;
-		  if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
-		  let is_toggle = _this.settings.toggle;
-		  if (el.classList.contains("is-toggle")) is_toggle = true;
-  
-		  // Loop through the accordion's sections and set up the click behavior
-		  const sections = el.getElementsByClassName("accordion");
-		  const all_toggles = el.getElementsByClassName("accordion-head");
-		  const all_contents = el.getElementsByClassName("accordion-body");
-		  for (let i = 0; i < sections.length; i++) {
-			const section = sections[i];
-			const toggle = all_toggles[i];
-			const content = all_contents[i];
-  
-			// Click behavior
-			toggle.addEventListener("click", function(e) {
-			  if (!is_toggle) {
-				// Hide all content areas first
-				for (let a = 0; a < all_contents.length; a++) {
-				  _this.closeAccordion(all_toggles[a], all_contents[a]);
-				}
-  
-				// Expand the clicked item
-				_this.openAccordion(toggle, content);
-			  } else {
-				// Toggle the clicked item
-				if (toggle.classList.contains("is-open")) {
-				  _this.closeAccordion(toggle, content);
-				} else {
-				  _this.openAccordion(toggle, content);
-				}
-			  }
-			});
-  
-			// Expand the first item
-			if (i === 0 && is_first_expanded) {
-			  _this.openAccordion(toggle, content);
+	var width = $(window).width();
+		$(window).resize(function() {
+			if (width > 992 && $(window).width() < 992) {
+				location.reload();
 			}
-		  }
-		}
-	  };
-  
-	  (function() {
-		// Initiate all instances on the page
-		const accordions = document.getElementsByClassName("accordions");
-		for (let i = 0; i < accordions.length; i++) {
-		  Accordion.init(accordions[i]);
-		}
-	  })();
-  
+			else if (width < 992 && $(window).width() > 992) {
+				location.reload();
+			}
+	})
 
 
 
@@ -132,23 +82,71 @@
 		}
 	});
 
-
 	$('.owl-features').owlCarousel({
-		center: true,
-      items:2,
-      loop:true,
-      nav: true,
-      margin:30,
-      responsive:{
-        992:{
-            items:3
-        },
-		1200:{
-			items:4
+		items:3,
+		loop:true,
+		dots: false,
+		nav: true,
+		autoplay: true,
+		margin:30,
+		responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:2
+			  },
+			  1200:{
+				  items:3
+			  },
+			  1800:{
+				items:3
+			}
 		}
-      }
-	});
+	})
 
+	$('.owl-collection').owlCarousel({
+		items:3,
+		loop:true,
+		dots: false,
+		nav: true,
+		autoplay: true,
+		margin:30,
+		responsive:{
+			  0:{
+				  items:1
+			  },
+			  800:{
+				  items:2
+			  },
+			  1000:{
+				  items:3
+			}
+		}
+	})
+
+	$('.owl-banner').owlCarousel({
+		items:1,
+		loop:true,
+		dots: false,
+		nav: true,
+		autoplay: true,
+		margin:30,
+		responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:1
+			  },
+			  1000:{
+				  items:1
+			}
+		}
+	})
+
+	
+	
 	
 
 	// Menu Dropdown Toggle
