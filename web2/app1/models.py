3e03960a0e2667dd9c662b2e0c9ba9c325d8d71a
ThanhHihi <thanhlm20@fpt.com.vn>
2023-02-10 19:40:10 +0700

    upload

--- web2/app1/models.py
+++
@@ -1,9 +0,0 @@
-from itertools import count
-from django.db import models
-
-# Create your models here.
-class AccessCount(models.Model):
-    count = models.IntegerField(default=0)
-
-    def add(self):
-        self.count += 1
